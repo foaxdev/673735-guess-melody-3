6576a4a26aec9358e0b6cf2f06de3fa40a863e16
イリーナ・コバルチュク <foaxdev@yahoo.com>
2020-02-06 10:43:43 +0300

    Adds basic components

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import ReactDOM from "react-dom";
+import {App} from "./components/app";
+import React from "react";
 
+const POSSIBLE_MISTAKES_QUANTITY = 5;
+
+ReactDOM.render(
+    <App mistakes={POSSIBLE_MISTAKES_QUANTITY}/>,
+    document.getElementById(`root`)
+);
